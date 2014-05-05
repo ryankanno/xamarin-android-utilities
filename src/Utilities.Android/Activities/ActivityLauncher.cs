67958e3652976e428ebca9f1a17347af0362eb4d
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-04 20:30:22 -0400

    FIRST. Added ActivityLauncher helper.

---
+++ src/Utilities.Android/Activities/ActivityLauncher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Utilities.Android.Activities
+{
+	using global::Android.App;
+	using global::Android.Content;
+	using global::Android.OS;
+	using Java.Lang;
+
+	public sealed class ActivityLauncher 
+	{
+    	private readonly Activity activity;
+
+	    public ActivityLauncher(Activity launcher) {
+	        this.activity = launcher;
+	    }
+	
+	    public void startActivity(Intent intent, int delayMillis = 0) {
+			new Handler().PostDelayed(new Runnable(delegate {
+				this.activity.StartActivity (intent);
+				this.activity.Finish ();
+	        }), delayMillis);
+	    }
+	}
+} 
