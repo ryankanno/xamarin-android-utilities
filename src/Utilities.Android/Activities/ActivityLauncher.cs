d5d5c6bbb2ab7c0017199af69b7b5723ab3479ac
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-04 20:32:13 -0400

    Fixed ActivityLauncher

--- src/Utilities.Android/Activities/ActivityLauncher.cs
+++ src/Utilities.Android/Activities/ActivityLauncher.cs
@@ -1,24 +1,26 @@
-﻿using System;
-namespace Utilities.Android.Activities
+﻿namespace Utilities.Android.Activities
 {
 	using global::Android.App;
 	using global::Android.Content;
 	using global::Android.OS;
 	using Java.Lang;
+	using System;
 
-	public sealed class ActivityLauncher 
+	public sealed class ActivityLauncher
 	{
-    	private readonly Activity activity;
+		private readonly Activity activity;
 
-	    public ActivityLauncher(Activity launcher) {
-	        this.activity = launcher;
-	    }
-	
-	    public void startActivity(Intent intent, int delayMillis = 0) {
-			new Handler().PostDelayed(new Runnable(delegate {
+		public ActivityLauncher (Activity launcher)
+		{
+			this.activity = launcher;
+		}
+
+		public void startActivity (Intent intent, int delayMillis = 0)
+		{
+			new Handler ().PostDelayed (new Runnable (delegate {
 				this.activity.StartActivity (intent);
 				this.activity.Finish ();
-	        }), delayMillis);
-	    }
+			}), delayMillis);
+		}
 	}
 } 
